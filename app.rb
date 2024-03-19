1e63237e3ea4d7ecf3b8722b066d5c1d85e9aff9
Maya Sheriff <113066754+MayaS1111@users.noreply.github.com>
2024-03-19 02:15:55 +0000

    game complete!

--- app.rb
+++ app.rb
@@ -7,17 +7,55 @@ get('/') do
 end
 
 get ("/rock") do
-  2player1 = "rock"
+  @player1 = "rock"
+  @result = "N/A"
   
   array = ["rock", "paper", "scissors"]
   @player2 = array.sample
-
   
+  if @player2 == @player1
+    @result = "tie"
+  elsif @player2 == "paper"  
+    @result = "lost"
+  else
+    @result = "won"
+  end
 
+  erb(:game_results, {:layout => :application_layout})
+end
 
-  2result = "N/A"
+get ("/paper") do
+  @player1 = "paper"
+  @result = "N/A"
+  
+  array = ["rock", "paper", "scissors"]
+  @player2 = array.sample
+  
+  if @player2 == @player1
+    @result = "tie"
+  elsif @player2 == "scissors"  
+    @result = "lost"
+  else
+    @result = "won"
+  end
 
+  erb(:game_results, {:layout => :application_layout})
+end
 
+get ("/scissors") do
+  @player1 = "scissors"
+  @result = "N/A"
+  
+  array = ["rock", "paper", "scissors"]
+  @player2 = array.sample
+  
+  if @player2 == @player1
+    @result = "tie"
+  elsif @player2 == "rock"  
+    @result = "lost"
+  else
+    @result = "won"
+  end
 
-  erb(:rock, {:layout => :application_layout})
+  erb(:game_results, {:layout => :application_layout})
 end
