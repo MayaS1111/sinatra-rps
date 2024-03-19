f991b770e4ff830927fb6eb426e3af2924ffbac7
Maya Sheriff <113066754+MayaS1111@users.noreply.github.com>
2024-03-19 01:59:04 +0000

    started on rock path in .rb

--- app.rb
+++ app.rb
@@ -7,5 +7,17 @@ get('/') do
 end
 
 get ("/rock") do
+  2player1 = "rock"
+  
+  array = ["rock", "paper", "scissors"]
+  @player2 = array.sample
+
+  
+
+
+  2result = "N/A"
+
+
+
   erb(:rock, {:layout => :application_layout})
 end
